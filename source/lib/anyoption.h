93c7c01b5d5aaef0b51c7d727600e7efbbe3e744
zeli <zeli@zarm.uni-bremen.de>
2017-08-08 14:55:05 +0200

    lic info removed

--- source/lib/anyoption.h
+++ source/lib/anyoption.h
@@ -1,22 +1,3 @@
-/* * atus-pro testing - atus-pro testing playgroung
- * Copyright (C) 2017 Želimir Marojević <zelimir.marojevic@gmail.com>
- *
- * This file is part of atus-pro testing.
- *
- * atus-pro testing is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * atus-pro testing is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with atus-pro testing.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifndef _ANYOPTION_H
 #define _ANYOPTION_H
 
