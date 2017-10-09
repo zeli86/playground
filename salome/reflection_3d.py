34dd0dad25dd98d5b19f11266ca6e0bfafc60df5
zeli <zeli@zarm.uni-bremen.de>
2017-10-09 10:53:24 +0200

    added lic info

--- salome/reflection_3d.py
+++ salome/reflection_3d.py
@@ -1,5 +1,26 @@
 # -*- coding: utf-8 -*-
 
+# ATUS-PRO - The ATUS-PRO package is atom interferometer Toolbox developed at ZARM
+# (CENTER OF APPLIED SPACE TECHNOLOGY AND MICROGRAVITY), Germany. This project is
+# founded by the DLR Agentur (Deutsche Luft und Raumfahrt Agentur). Grant numbers:
+# 50WM0942, 50WM1042, 50WM1342.
+# Copyright (C) 2017 Želimir Marojević, Ertan Göklü, Claus Lämmerzahl
+#
+# This file is part of ATUS-PRO.
+#
+# ATUS-PRO is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ATUS-PRO is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ATUS-PRO.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import salome
 
