bcaa521b4c4cbe2b5addcb4589b262cf3cc17fa9
zeli <zeli@zarm.uni-bremen.de>
2017-11-29 21:41:47 +0100

    2d, 3d instanzierung

--- source/groundstates/my_newton_mpi.cpp
+++ source/groundstates/my_newton_mpi.cpp
@@ -18,9 +18,6 @@
 // along with atus-pro testing.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-/** Želimir Marojević
- */
-
 #include <deal.II/lac/generic_linear_algebra.h>
 
 namespace LA
@@ -87,6 +84,7 @@ namespace LA
 #include "MyParameterHandler.h"
 #include "MyRealTools.h"
 #include "MyComplexTools.h"
+#include "muParser.h"
 
 namespace BreedSolver
 {
@@ -963,10 +961,34 @@ int main ( int argc, char *argv[] )
   using namespace dealii;
   //deallog.depth_console (2);
 
+  MyParameterHandler params("params.xml");
+  int dim=0;
+
+  try
+  {
+    dim = int(params.Get_Mesh("DIM",0));
+  }
+  catch (mu::Parser::exception_type &e)
+  {
+    cout << "Message:  " << e.GetMsg() << "\n";
+    cout << "Formula:  " << e.GetExpr() << "\n";
+    cout << "Token:    " << e.GetToken() << "\n";
+    cout << "Position: " << e.GetPos() << "\n";
+    cout << "Errc:     " << e.GetCode() << "\n";
+  }
+
   Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv );
   {
-    BreedSolver::MySolver<DIMENSION> solver("params.xml");
-    solver.run2b();
+    switch(dim)
+    {
+      case 2: { BreedSolver::MySolver<2> solver("params.xml");
+                solver.run2b();
+                break; }
+      case 3: { BreedSolver::MySolver<3> solver("params.xml");
+                solver.run2b();
+                break; }
+      default: cout << "You have found a new dimension!" << endl;
+    }
   }
 return EXIT_SUCCESS;
 }
