385fa0af0021352aff766e6b7f8ca80d7d028eb9
zeli <zeli@zarm.uni-bremen.de>
2017-12-09 19:18:21 +0100

    improved

--- source/case_gen/batch_case_generation_xml.cpp
+++ source/case_gen/batch_case_generation_xml.cpp
@@ -26,8 +26,11 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <functional>
 
 #include "pugixml.hpp"
+#include "anyoption.h"
 
 using namespace std;
 
@@ -37,14 +40,84 @@ const long Ndmu = 40;
 const double dmu = .02;
 double omega[] = {0.5, 0.5, 0.5};
 
+std::string qn_to_str_1D( const int l, const int /*maxN*/ )
+{
+  return std::to_string(l) + ",0,0"; 
+}
+
+std::string qn_to_str_2D( const int l, const int maxN )
+{
+  int I = l / maxN; 
+  int J = l - I*maxN;
+  return std::to_string(I) + "," + std::to_string(J) + ",0"; 
+}
+
+std::string qn_to_str_3D( const int l, const int maxN )
+{
+  int I = l / (maxN * maxN); 
+  int J = l / maxN - I*maxN;
+  int K = l - I * (maxN * maxN) - J * maxN;
+  return std::to_string(I) + "," + std::to_string(J) + "," + std::to_string(K); 
+}
+
+std::string qn_to_path_1D( const int l, const int /*maxN*/ )
+{
+  return std::to_string(l); 
+}
+
+std::string qn_to_path_2D( const int l, const int maxN )
+{
+  int I = l / maxN; 
+  int J = l - I*maxN;
+  return std::to_string(I) + "_" + std::to_string(J); 
+}
+
+std::string qn_to_path_3D( const int l, const int maxN )
+{
+  int I = l / (maxN * maxN); 
+  int J = l / maxN - I*maxN;
+  int K = l - I * (maxN * maxN) - J * maxN;
+  return std::to_string(I) + "_" + std::to_string(J) + "_" + std::to_string(K); 
+}
+
 int main( int argc, char *argv[] )
 {
+  AnyOption * opt = new AnyOption();
+  int dim=1, N=4, r=9;
+
+  opt->addUsage( "" );
+  opt->addUsage( "Usage: batch_case_generation [options]" );
+  opt->addUsage( "" );
+  opt->addUsage( " --help  Prints this help " );
+  opt->addUsage( " --dim   1 or 2 or 3" );
+  opt->addUsage( " --N     integer" );
+  opt->addUsage( " --r     global refinement" );
+  opt->addUsage( "" );
+  opt->setFlag(  "help" );   
+  opt->setOption( "dim" );   
+  opt->setOption( "N" );   
+  opt->setOption( "r" );   
+  
+  opt->processCommandArgs( argc, argv );
+
+  if( opt->getFlag( "help" ) || opt->getFlag( 'h' ) ) opt->printUsage();
+
+  if( opt->getValue("dim") != nullptr ) 
+    dim = atof(opt->getValue("dim"));
+
+  if( opt->getValue("N") != nullptr ) 
+    N = atof(opt->getValue("N"));
+
+  if( opt->getValue("r") != nullptr ) 
+    r = atof(opt->getValue("r"));
+
+  delete opt; 
+
   const string HomePath = getenv ("HOME");
 
   string tmpstr;
 
   char base_folder[255];
-  char folder[255];
   char filename[255];
   char shellcmd[255];
   char datetime[255];
@@ -56,6 +129,15 @@ int main( int argc, char *argv[] )
   timeinfo = localtime(&rawtime);
   strftime(datetime,255,"%F_%R",timeinfo);
 
+  std::map<int,std::function<std::string(const int,const int)>> qn_map;
+  qn_map[1] = qn_to_str_1D; 
+  qn_map[2] = qn_to_str_2D; 
+  qn_map[3] = qn_to_str_3D;
+  std::map<int,std::function<std::string(const int,const int)>> path_map;
+  path_map[1] = qn_to_path_1D; 
+  path_map[2] = qn_to_path_2D; 
+  path_map[3] = qn_to_path_3D;
+  
 #if POTENTIAL==1
   sprintf( base_folder, "HTRAP_%s", datetime );
 #endif
@@ -63,223 +145,84 @@ int main( int argc, char *argv[] )
   sprintf( base_folder, "GOST_%s", datetime );
   omega[0] = 0.5039287608;
 #endif
-  
-#if DIMENSION==2  
-  for( long i1=0; i1<N1[0]; i1++ )
+
+  const int maxN = int(pow(N,dim));
+  for( int s=0; s<maxN; s++ )
   {
-    for( long j1=0; j1<N1[1]; j1++ )
-    {
-      sprintf( folder, "%ld_%ld", i1, j1 );
-      sprintf( shellcmd, "mkdir -p %s/%s", base_folder, folder );
-      system( shellcmd );
+    sprintf( shellcmd, "mkdir -p %s/%s", base_folder, path_map[dim](s,N).c_str() );
+    system( shellcmd );
+    sprintf( filename, "%s/%s/params.xml", base_folder, path_map[dim](s,N).c_str() );
 
-      sprintf( filename, "%s/%s/params.xml", base_folder, folder );
+    pugi::xml_document doc;
+    pugi::xml_node parameter_node = doc.append_child("PARAMETER");
+    pugi::xml_node physics_node = parameter_node.append_child("PHYSICS");
+    pugi::xml_node mesh_node = parameter_node.append_child("MESH");
+    pugi::xml_node algorithm_node = parameter_node.append_child("ALGORITHM");      
 
-      pugi::xml_document doc;
-      pugi::xml_node parameter_node = doc.append_child("PARAMETER");
-      pugi::xml_node physics_node = parameter_node.append_child("PHYSICS");
-      pugi::xml_node mesh_node = parameter_node.append_child("MESH");
-      pugi::xml_node algorithm_node = parameter_node.append_child("ALGORITHM");      
+    // PARAMETER childs
+    pugi::xml_node node = parameter_node.append_child("FILENAME");
+    node.append_child(pugi::node_pcdata).set_value("final.bin");
 
-      // PARAMETER childs
-      pugi::xml_node node = parameter_node.append_child("FILENAME");
-      node.append_child(pugi::node_pcdata).set_value("final.bin");
+    node = parameter_node.append_child("guess_fct");
+    node.append_child(pugi::node_pcdata).set_value("exp(-x^2-y^2-z^2)");
 
-      node = parameter_node.append_child("guess_fct");
-      node.append_child(pugi::node_pcdata).set_value("exp(-x^2-y^2)");
+    // PHYSICS childs
+    node = physics_node.append_child("QN1");
+    node.append_child(pugi::node_pcdata).set_value(qn_map[dim](s,N).c_str());
 
-      // PHYSICS childs
-      node = physics_node.append_child("QN1");
-      tmpstr = to_string(i1) + "," + to_string(j1) + ",0";
-      node.append_child(pugi::node_pcdata).set_value(tmpstr.c_str());
+    node = physics_node.append_child("omega");
+    tmpstr = to_string(omega[0]) + "," + to_string(omega[1]) + "," + to_string(omega[2]);
+    node.append_child(pugi::node_pcdata).set_value(tmpstr.c_str());
 
-      node = physics_node.append_child("omega");
-      tmpstr = to_string(omega[0]) + "," + to_string(omega[1]) + "," + to_string(omega[2]);
-      node.append_child(pugi::node_pcdata).set_value(tmpstr.c_str());
+    node = physics_node.append_child("gs_1");
+    node.append_child(pugi::node_pcdata).set_value("1,1");
 
-      node = physics_node.append_child("gs_1");
-      node.append_child(pugi::node_pcdata).set_value("1,1");
+    node = physics_node.append_child("mu");
+    node.append_child(pugi::node_pcdata).set_value("5,5");
 
-      node = physics_node.append_child("mu");
-      node.append_child(pugi::node_pcdata).set_value("5,5");
-
-      // MESH childs
-      node = mesh_node.append_child("DIM");
-      node.append_child(pugi::node_pcdata).set_value("2");
+    // MESH childs
+    node = mesh_node.append_child("DIM");
+    node.append_child(pugi::node_pcdata).set_value("3");
 #if POTENTIAL==1
-      node = mesh_node.append_child("xrange");
-      node.append_child(pugi::node_pcdata).set_value("-10,10");
+    node = mesh_node.append_child("xrange");
+    node.append_child(pugi::node_pcdata).set_value("-10,10");
 #endif 
 #if POTENTIAL==2
-      node = mesh_node.append_child("xrange");
-      node.append_child(pugi::node_pcdata).set_value("0,20");
+    node = mesh_node.append_child("xrange");
+    node.append_child(pugi::node_pcdata).set_value("0,20");
 #endif 
-      node = mesh_node.append_child("yrange");
-      node.append_child(pugi::node_pcdata).set_value("-10,10");
+    node = mesh_node.append_child("yrange");
+    node.append_child(pugi::node_pcdata).set_value("-10,10");
 
-      node = mesh_node.append_child("zrange");
-      node.append_child(pugi::node_pcdata).set_value("-15,15");
+    node = mesh_node.append_child("zrange");
+    node.append_child(pugi::node_pcdata).set_value("-10,10");
 
-      node = mesh_node.append_child("global_refinements");
-      node.append_child(pugi::node_pcdata).set_value("9");
+    node = mesh_node.append_child("global_refinements");
+    node.append_child(pugi::node_pcdata).set_value(to_string(r).c_str());
 
-      // ALGORTHM childs
-      node = algorithm_node.append_child("ti");
-      node.append_child(pugi::node_pcdata).set_value("1");
+    // ALGORTHM childs
+    node = algorithm_node.append_child("ti");
+    node.append_child(pugi::node_pcdata).set_value("1");
 
-      node = algorithm_node.append_child("NA");
-      node.append_child(pugi::node_pcdata).set_value("1000");
+    node = algorithm_node.append_child("NA");
+    node.append_child(pugi::node_pcdata).set_value("1000");
 
-      node = algorithm_node.append_child("NK");
-      node.append_child(pugi::node_pcdata).set_value("10");
+    node = algorithm_node.append_child("NK");
+    node.append_child(pugi::node_pcdata).set_value("10");
 
-      node = algorithm_node.append_child("dt");
-      node.append_child(pugi::node_pcdata).set_value("0.001");
+    node = algorithm_node.append_child("dt");
+    node.append_child(pugi::node_pcdata).set_value("0.001");
 
-      node = algorithm_node.append_child("epsilon");
-      node.append_child(pugi::node_pcdata).set_value("1e-5,1e-10");
+    node = algorithm_node.append_child("epsilon");
+    node.append_child(pugi::node_pcdata).set_value("1e-5,1e-10");
 
-      node = algorithm_node.append_child("dmu");
-      tmpstr = to_string(dmu);
-      node.append_child(pugi::node_pcdata).set_value(tmpstr.c_str());
+    node = algorithm_node.append_child("dmu");
+    tmpstr = to_string(dmu);
+    node.append_child(pugi::node_pcdata).set_value(tmpstr.c_str());
 
-      node = algorithm_node.append_child("Ndmu");
-      tmpstr = to_string(Ndmu);
-      node.append_child(pugi::node_pcdata).set_value(tmpstr.c_str());
-
-      // add param node before the description
-      //pugi::xml_node param = node.insert_child_before("param", descr);
-
-      // add attributes to param node
-      //param.append_attribute("name") = "version";
-      //param.append_attribute("value") = 1.1;
-      //param.insert_attribute_after("type", param.attribute("name")) = "float";
-
-      cout << "Saving result: " << doc.save_file(filename) << endl;
-/*
-      sprintf( filename, "%s/%s.pbs", base_folder, folder );
-      ofstream pbs_file( filename );
-      pbs_file << "#!/bin/bash\n";
-      pbs_file << "#PBS -M zeli@zarm.uni-bremen.de\n";
-      pbs_file << "#PBS -N " << folder << "\n";
-      pbs_file << "#PBS -j oe\n";
-      pbs_file << "#PBS -l walltime=10:00:00\n"; // our cluster has a max of 600:00:00
-      pbs_file << "#PBS -l nodes=1:ppn=8\n";
-      pbs_file << "#PBS -V\n";
-      pbs_file << "cd " << HomePath << "/" << base_folder << "/" << folder << "\n";
-      pbs_file << "mpirun my_solver_mpi\n";
-      //pbs_file << "mpirun --map-by core my_csolver_mpi\n";
-      pbs_file.close();
-*/
-/*
-      string jobname = "breed-" + to_string(i1) + "_" + to_string(j1);
-
-      string filename1 = jobname + "-job.sh";
-      ofstream slurm_file( filename1 );
-      slurm_file << "#!/bin/bash\n";
-      slurm_file << "#SBATCH -J " << jobname << "\n";
-      slurm_file << "#SBATCH -N 1 -n 8\n";
-      slurm_file << "#SBATCH --mail-user=zeli@zarm.uni-bremen.de\n";
-      slurm_file << "#SBATCH --mail-type=BEGIN --mail-type=END\n";
-      slurm_file << "#SBATCH -o matrixhell-%j.out\n";
-
-      slurm_file << "export MODULEPATH=$MODULEPATH:/home/zeli/local/modules/modulefiles\n";
-      slurm_file << "module load GCC/5.4.0\n";
-      slurm_file << "module load Cmake/3.7.0\n";
-      slurm_file << "module load muparser-2.2.5\n";
-      slurm_file << "module load atus-pro-git-meta\n";
-      slurm_file << "cd " << HomePath + "/" + base_folder + "/" + folder << "\n";
-      slurm_file << "mpirun -np 8 my_newton_mpi\n";
-      slurm_file.close();
-*/
-    }
-  }
-#endif
-
-#if DIMENSION==3 
-for( long i1=0; i1<N1[0]; i1++ )
-{
-  for( long j1=0; j1<N1[1]; j1++ )
-  {
-    for( long k1=0; k1<=j1; k1++ )
-    {
-      sprintf( folder, "%ld_%ld_%ld", i1, j1, k1 );
-      sprintf( shellcmd, "mkdir -p %s/%s", base_folder, folder );
-      system( shellcmd );
-
-      sprintf( filename, "%s/%s/params.xml", base_folder, folder );
-
-      pugi::xml_document doc;
-      pugi::xml_node parameter_node = doc.append_child("PARAMETER");
-      pugi::xml_node physics_node = parameter_node.append_child("PHYSICS");
-      pugi::xml_node mesh_node = parameter_node.append_child("MESH");
-      pugi::xml_node algorithm_node = parameter_node.append_child("ALGORITHM");      
-
-      // PARAMETER childs
-      pugi::xml_node node = parameter_node.append_child("FILENAME");
-      node.append_child(pugi::node_pcdata).set_value("final.bin");
-
-      node = parameter_node.append_child("guess_fct");
-      node.append_child(pugi::node_pcdata).set_value("exp(-x^2-y^2-z^2)");
-
-      // PHYSICS childs
-      node = physics_node.append_child("QN1");
-      tmpstr = to_string(i1) + "," + to_string(j1) + "," + to_string(k1);
-      node.append_child(pugi::node_pcdata).set_value(tmpstr.c_str());
-
-      node = physics_node.append_child("omega");
-      tmpstr = to_string(omega[0]) + "," + to_string(omega[1]) + "," + to_string(omega[2]);
-      node.append_child(pugi::node_pcdata).set_value(tmpstr.c_str());
-
-      node = physics_node.append_child("gs_1");
-      node.append_child(pugi::node_pcdata).set_value("1,1");
-
-      node = physics_node.append_child("mu");
-      node.append_child(pugi::node_pcdata).set_value("5,5");
-
-      // MESH childs
-      node = mesh_node.append_child("DIM");
-      node.append_child(pugi::node_pcdata).set_value("3");
-#if POTENTIAL==1
-      node = mesh_node.append_child("xrange");
-      node.append_child(pugi::node_pcdata).set_value("-10,10");
-#endif 
-#if POTENTIAL==2
-      node = mesh_node.append_child("xrange");
-      node.append_child(pugi::node_pcdata).set_value("0,20");
-#endif 
-      node = mesh_node.append_child("yrange");
-      node.append_child(pugi::node_pcdata).set_value("-10,10");
-
-      node = mesh_node.append_child("zrange");
-      node.append_child(pugi::node_pcdata).set_value("-10,10");
-
-      node = mesh_node.append_child("global_refinements");
-      node.append_child(pugi::node_pcdata).set_value("3");
-
-      // ALGORTHM childs
-      node = algorithm_node.append_child("ti");
-      node.append_child(pugi::node_pcdata).set_value("1");
-
-      node = algorithm_node.append_child("NA");
-      node.append_child(pugi::node_pcdata).set_value("1000");
-
-      node = algorithm_node.append_child("NK");
-      node.append_child(pugi::node_pcdata).set_value("10");
-
-      node = algorithm_node.append_child("dt");
-      node.append_child(pugi::node_pcdata).set_value("0.001");
-
-      node = algorithm_node.append_child("epsilon");
-      node.append_child(pugi::node_pcdata).set_value("1e-5,1e-10");
-
-      node = algorithm_node.append_child("dmu");
-      tmpstr = to_string(dmu);
-      node.append_child(pugi::node_pcdata).set_value(tmpstr.c_str());
-
-      node = algorithm_node.append_child("Ndmu");
-      tmpstr = to_string(Ndmu);
-      node.append_child(pugi::node_pcdata).set_value(tmpstr.c_str());
+    node = algorithm_node.append_child("Ndmu");
+    tmpstr = to_string(Ndmu);
+    node.append_child(pugi::node_pcdata).set_value(tmpstr.c_str());
 
     // add param node before the description
     //pugi::xml_node param = node.insert_child_before("param", descr);
@@ -290,47 +233,6 @@ for( long i1=0; i1<N1[0]; i1++ )
     //param.insert_attribute_after("type", param.attribute("name")) = "float";
 
       cout << "Saving result: " << doc.save_file(filename) << endl;
-/*
-    sprintf( filename, "%s/%s.pbs", base_folder, folder );
-    ofstream pbs_file( filename );
-    pbs_file << "#!/bin/bash\n";
-    pbs_file << "#PBS -M zeli@zarm.uni-bremen.de\n";
-    pbs_file << "#PBS -N " << folder << "\n";
-    pbs_file << "#PBS -j oe\n";
-    pbs_file << "#PBS -l walltime=10:00:00\n"; // our cluster has a max of 600:00:00
-    pbs_file << "#PBS -l nodes=1:ppn=8\n";
-    pbs_file << "#PBS -V\n";
-    pbs_file << "cd " << HomePath << "/" << base_folder << "/" << folder << "\n";
-    pbs_file << "mpirun my_solver_mpi\n";
-    //pbs_file << "mpirun --map-by core my_csolver_mpi\n";
-    pbs_file.close();
-*/
-
-    string jobname = "breed-" + to_string(i1) + "_" + to_string(j1) + "_" + to_string(k1);
-
-    string filename1 = jobname + "-job.sh";
-    ofstream slurm_file( filename1 );
-    slurm_file << "#!/bin/bash\n";
-    slurm_file << "#SBATCH -J " << jobname << "\n";
-    slurm_file << "#SBATCH -N 1 -n 8\n";
-    slurm_file << "#SBATCH --mail-user=zeli@zarm.uni-bremen.de\n";
-    slurm_file << "#SBATCH --mail-type=BEGIN --mail-type=END\n";
-    slurm_file << "#SBATCH -o matrixhell-%j.out\n";
-
-    slurm_file << "export MODULEPATH=$MODULEPATH:/home/zeli/local/modules/modulefiles\n";
-    slurm_file << "module load GCC/5.4.0\n";
-    slurm_file << "module load Cmake/3.7.0\n";
-    slurm_file << "module load muparser-2.2.5\n";
-    slurm_file << "module load atus-pro-git-meta\n";
-    slurm_file << "cd " << HomePath + "/" + base_folder + "/" + folder << "\n";
-    slurm_file << "mpirun -np 8 my_newton_mpi\n";
-    slurm_file.close();
-    }
   }
+return EXIT_SUCCESS;
 }
-#endif
-
-return 0;
-}
-
-
