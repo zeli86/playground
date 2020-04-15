7fa6f3c7d5b5171d3047f39740b2c1c81010c326
Zelimir Marojevic <zelimir.marojevic@gmail.com>
2020-04-15 18:27:26 +0200

    test 3

--- auto_install_9_1_1.sh
+++ auto_install_9_1_1.sh
@@ -2,7 +2,7 @@
 
 set timeout -1
 
-spawn ./install_9_1_1.lisp --build-dir=/home/zeli/local/src/ --install-dir=/home/zeli/local/opt/ --module-dir=/home/zeli/local/modules/modulefiles/ -j 2
+spawn ./install_9_1_1.lisp --build-dir=/home/zeli/local/src/ --install-dir=/home/zeli/local/opt/ --module-dir=/home/zeli/local/modules/modulefiles/ --debug -j 2
 
 expect " - Abort Installation.\r"
 
